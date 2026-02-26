24b4cd2636062bbd2d13b71af48a0325aaa38bff
Vincent Mitard <vincent.mitard@gmail.com>
2026-02-26 08:05:32 +0000

    Mise à jour des VNets pour le TP BGP1

--- backupAnsibleEnvironment.sh
+++ backupAnsibleEnvironment.sh
@@ -10,8 +10,6 @@ cd ~
 echo "-I- Création d'une archive de la configuration Ansible"
 echo "-I- Sauvegarde de la clé privée de l'utilisateur"
 tar --absolute-name --create --file AnsibleEnv.tar $HOME/.ssh/id_rsa
-echo "-I- Sauvegarde de fichier de configuration pour l'authentification avec l'hyperviseur"
-tar --absolute-name --append --file AnsibleEnv.tar $HOME/pveAuthentication.yml
 echo "-I- Sauvegarde de fichier HOST"
 tar --absolute-name --append --file AnsibleEnv.tar $HOME/.ansible/hostsPod
 echo "-I- Sauvegarde des roles"

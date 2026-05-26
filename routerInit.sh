ad159b87af18dad66901ed07cfebc17507b6d84e
Vincent Mitard <vincent.mitard@gmail.com>
2026-05-26 18:49:44 +0000

    Mise à jour des paramètres pour l'initialisation du lab OSPFv2

--- routerInit.sh
+++ routerInit.sh
@@ -6,7 +6,7 @@
 # 2026-02-15 - Mitard V. : Ajout des configurations des labs BGP
 # 2026-03-11 - Mitard V. : Suppression de l'initialisation du fichier d'authentification (Initialisé par défaut dans le .profile de l'utilisateur)
 # 2026-05-23 - Mitard V. : Ajout d'une 3ème topologie pour les TPs BGP (BGP3)
-# 2026-05-24 - Mitard V. : Ajout d'une topologie pour le TP OSPF
+# 2026-05-24 - Mitard V. : Ajout d'une topologie pour le TP OSPFv2
 #
 #
 scriptName=`basename $0`
@@ -28,7 +28,7 @@ while getopts "dDe:hH" opt; do
        ;;
     h|H) tput setaf 3
 	 echo -e "\n-I- $scriptName permet d'initialiser la configuration l'ensemble des routeurs d'un Pod."
-	 echo -e "-I- $scriptName [-d|-D] [-h|-H] [-e <Fichier d'authentification>] RAZ|BGP<n>|L2VPN|L3VPN|OSPF|PW [<No de Pod>]"
+	 echo -e "-I- $scriptName [-d|-D] [-h|-H] [-e <Fichier d'authentification>] RAZ|BGP<n>|L2VPN|L3VPN|OSPFv2|PW [<No de Pod>]"
 	 echo -e "\t-d|-D : Activativation des traces de débogage."
 	 echo -e "\t-h|-H : Affichage de cette aide en ligne."
 	 echo -e "\t-e : Nom du fichier contenant les informations d'authentification pour l'accès au noeud Proxmox.\n"
@@ -55,46 +55,46 @@ case $# in
 esac
 
 case ${1^^} in
-  RAZ)   playbook=$playbooks/linux-routers/clearFRRconf.yml
-         ;;
+  RAZ)    playbook=$playbooks/linux-routers/clearFRRconf.yml
+          ;;
   # TP routage OSPF
-  BGP1)  playbook=$playbooks/linux-routers/copyFRRconf.yml
-         configuration=_OSPF
-         targets=OSPF_"$Pod"RTR
-         ;;
+  OSPFV2) playbook=$playbooks/linux-routers/copyFRRconf.yml
+          configuration=_OSPFv2_lab
+          targets=OSPF_"$Pod"RTR
+          ;;
   # TP routage BGP
-  BGP1)  playbook=$playbooks/linux-routers/copyFRRconf.yml
-         configuration=_BGP_TP1
-         targets=BGP1_"$Pod"RTR
-         ;;
-  BGP2)  playbook=$playbooks/linux-routers/copyFRRconf.yml
-         configuration=_BGP_TP2
-         targets=BGP2_"$Pod"RTR
-         ;;
-  BGP3)  playbook=$playbooks/linux-routers/copyFRRconf.yml
-         configuration=_BGP_TP3
-         targets=BGP3_"$Pod"RTR
-         ;;
+  BGP1)   playbook=$playbooks/linux-routers/copyFRRconf.yml
+          configuration=_BGP_TP1
+          targets=BGP1_"$Pod"RTR
+          ;;
+  BGP2)   playbook=$playbooks/linux-routers/copyFRRconf.yml
+          configuration=_BGP_TP2
+          targets=BGP2_"$Pod"RTR
+          ;;
+  BGP3)   playbook=$playbooks/linux-routers/copyFRRconf.yml
+          configuration=_BGP_TP3
+          targets=BGP3_"$Pod"RTR
+          ;;
   # TP IP/MPLS
-  BGP)   playbook=$playbooks/linux-routers/copyFRRconf.yml
-         configuration=_MPLS_lab1
-         targets=MPLS_"$Pod"RTR
-         ;;
-  PW)    playbook=$playbooks/linux-routers/copyFRRconf.yml
-         configuration=_PW
-         targets="$Pod"RTR
-         ;;
-  L2VPN) playbook=$playbooks/linux-routers/copyFRRconf.yml
-         configuration=_VPLS
-         targets="$Pod"RTR
-         ;;
-  L3VPN) playbook=$playbooks/linux-routers/copyFRRconf.yml
-         configuration=_MPLS_lab2
-         targets=MPLS_"$Pod"RTR
-         ;;
-  *) tput setaf 1; echo -e "\n-E- Option $1 invalide !\n"; tput sgr0
-     exit 1
-     ;;
+  BGP)    playbook=$playbooks/linux-routers/copyFRRconf.yml
+          configuration=_MPLS_lab1
+          targets=MPLS_"$Pod"RTR
+          ;;
+  PW)     playbook=$playbooks/linux-routers/copyFRRconf.yml
+          configuration=_PW
+          targets="$Pod"RTR
+          ;;
+  L2VPN)  playbook=$playbooks/linux-routers/copyFRRconf.yml
+          configuration=_VPLS
+          targets="$Pod"RTR
+          ;;
+  L3VPN)  playbook=$playbooks/linux-routers/copyFRRconf.yml
+          configuration=_MPLS_lab2
+          targets=MPLS_"$Pod"RTR
+          ;;
+  *)      tput setaf 1; echo -e "\n-E- Option $1 invalide !\n"; tput sgr0
+          exit 1
+          ;;
 esac
 
 edges=Pod$2Edge
@@ -103,4 +103,4 @@ ansible-playbook -i $ansibleHosts $ansibleDebug -e @$authenticationFile -e "targ
 ansible-playbook -i $ansibleHosts $ansibleDebug -e @$authenticationFile -e "target=$targets cmd=reboot" $playbooks/linux-routers/execCommand.yml
 if [ "${1^^}" = "L3VPN" ]; then
   ansible-playbook -i $ansibleHosts $ansibleDebug -e @$authenticationFile -e "target=$edges cmd=/usr/local/bin/clients-vrf-conf.sh" $playbooks/linux-routers/execCommand.yml
-fi
+i

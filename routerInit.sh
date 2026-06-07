c20706a51c5fc38f9b9599956999f644ec89e710
Vincent Mitard <vincent.mitard@gmail.com>
2026-06-07 09:32:03 +0000

    Correction erreur d'initialisation de la variable edges dans le script routerInit.sh

--- routerInit.sh
+++ routerInit.sh
@@ -97,7 +97,7 @@ case ${1^^} in
           ;;
 esac
 
-edges=Pod$2Edge
+edges="$Pod"Edge
 
 ansible-playbook -i $ansibleHosts $ansibleDebug -e @$authenticationFile -e "target=$targets suffix=$configuration" $playbook
 ansible-playbook -i $ansibleHosts $ansibleDebug -e @$authenticationFile -e "target=$targets cmd=reboot" $playbooks/linux-routers/execCommand.yml
